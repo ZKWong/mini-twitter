7f4229cbecb2a1a1c42e4bac19413ce9a3381d9e
ZKWong <zhikang.wong@siu.edu>
2018-05-23 16:59:17 +0800

    Update mini-twitter.go
    Added register / sign up function
    new user will have their username and password saved in a file named tbusers (testing purpose)
    If the username and password matched, user will be directed to homepage.html from index.html after login successfully
    Similar username will be prompted as existed.

--- mini-twitter.go
+++ mini-twitter.go
@@ -4,98 +4,138 @@ import (
     "fmt"
     "log"
     "net/http"
-    "io/ioutil"
+		"io/ioutil"
+		"bufio"
+		"os"
+		"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func getfile(sFile string, sHtml *string){
+	b, err := ioutil.ReadFile(sFile)
+	if err != nil{
+		fmt.Print(err)
+	}
+	*sHtml = string(b)
+	return
+}
+
+func checklogin_page(r *http.Request, sHtml *string) {
+	var username string
+	var password string
+	var querystr []string
+	var key_value []string
+	var usrname string
+	var pwd string
+
+	*sHtml = "0" 
+
+	r.ParseForm()
+	for key, value:= range r.Form{
+		if key == "username" {username = fmt.Sprintf("%s",value)}
+		if key == "password" {password = fmt.Sprintf("%s",value)}
+	}
+
+	file,err := os.Open("tbusers")
+	if err != nil {
+		fmt.Print(err)
+	}	
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		querystr  = strings.Split(scanner.Text(),"&")
+		key_value = strings.Split(querystr[0],"=")
+		if key_value[0]=="username" {usrname=key_value[1]}
+		key_value = strings.Split(querystr[1],"=")
+		if key_value[0]=="password" {pwd=key_value[1]}
+		if username==usrname{
+			if password==pwd {
+				*sHtml = "1"
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func new_signup_page(r *http.Request, sHtml *string) {  //Check if input is empty or not, prompt user to key in value
+	var new_username string
+	var new_password string
+	var querystr []string
+	var key_value []string
+	var new_usrname string
+	//var new_pwd string
+
+	*sHtml = "Sign up failed " + new_username +" "+  new_password
+
+	r.ParseForm()
+	for key, value:= range r.Form{
+		if key == "new_username" {new_username = fmt.Sprintf("%s",value)}
+		if key == "new_password" {new_password = fmt.Sprintf("%s",value)}
+	}
+	//fmt.Printf("new_username: %s  new_password: %s\n",new_username,new_password)
+
+	file,err := os.OpenFile("tbusers", os.O_APPEND|os.O_RDWR|os.O_CREATE,0644)//,err
+	if err != nil {fmt.Print(err)}	
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			querystr  = strings.Split(scanner.Text(),"&")
+			key_value = strings.Split(querystr[0],"=")
+			if key_value[0]=="username" {new_usrname=key_value[1]}
+			//fmt.Printf("%s %s %s %s\n",key_value[0],key_value[1],new_username,new_usrname)
+			//key_value = strings.Split(querystr[1],"=")
+			//if key_value[0]=="new_password" {new_pwd=key_value[1]}
+			if new_username==new_usrname{
+				*sHtml = "Username already exist"
+				return
+			}
+		}
+	
+	if _, err := file.Write([]byte("username=" + new_username + "&password=" + new_password +"\n")); err != nil{
+		fmt.Print(err)
+	}
+	if err := file.Close(); err != nil{
+		fmt.Print(err)
+	}
+	
+	*sHtml = "Sign Up successful"
+	return
+}
+
+
+func web_response(w http.ResponseWriter, r *http.Request) {
 		var sHtml string
-		if r.URL.Path[1:] == "vue2516.js" {
-			b, err := ioutil.ReadFile("vue2516.js")
-			if err != nil{
-				fmt.Print(err)
-			}
-			sHtml := string(b)
-			fmt.Fprintf(w,"%s",sHtml)
-			return
+		
+		switch r.URL.Path[1:] {
+			case "vue2516.js": 
+				getfile("vue2516.js",&sHtml)
+			
+			case "login.css" :
+				w.Header().Add("Content-Type","text/css")
+				getfile("login.css",&sHtml)
+			
+			case "checklogin":
+				checklogin_page(r, &sHtml)
+
+			case "new_signup":
+				new_signup_page(r, &sHtml)
+
+			case "homepage.html":
+				getfile("homepage.html",&sHtml)
+	
+			case "homepage.css" :
+				w.Header().Add("Content-Type","text/css")
+				getfile("homepage.css",&sHtml)
+
+			default:
+				getfile("index.html",&sHtml)
 		}
-		if r.URL.Path[1:] == "login.css" {
-			b, err := ioutil.ReadFile("login.css")
-			if err != nil{
-				fmt.Print(err)
-			}
-			sHtml := string(b)
-			w.Header().Add("Content-Type","text/css")
-			fmt.Fprintf(w,"%s",sHtml)
-			return
-		}
-		if r.URL.Path[1:] == "checklogin" {
-			var username string
-			var password string
-			r.ParseForm()
-			for key, value:= range r.Form{
-				if key == "username" {
-					username = fmt.Sprintf("%s",value)
-				}
-				if key == "password" {
-					password = fmt.Sprintf("%s",value)
-				}
-			}
-			sHtml = "login failed " + username +" "+  password
-			if username=="[test]"{
-				if password=="[1234]" {
-					sHtml = "login successful"
-				}
-			}
-			fmt.Fprintf(w,"%s",sHtml)
-			return
-		}
-		sHtml = "<!DOCTYPE html>\n" +
-						"<html>\n" +
-						"<head>\n"+
-						"<title>Mini-Twitter</title>\n"+
-						"<script src='vue2516.js'></script>\n"+
-						"<link rel ='stylesheet' type = 'text/css' href = 'login.css'/>\n"+
-						"</head>\n"+
-						"<div id='container'>\n"  +
-						"<div id='main'>\n"  +
-						"<h1>Mini Twitter</h1>\n"+
-						" Username:&#160; <input v-model = 'username' >\n"+
-						" Password:&#160; <input v-model = 'password' type ='password'>\n"+
-						"<button v-on:click='login()' id = 'login'>Login</button>\n"+
-						"</div>\n" +
-						"</div>\n" +
-						"</html>\n" +
-						"<script>\n" +
-						"	var main = new Vue({\n" +
-						"   el: '#main',\n" + 
-						"   data: {\n" +
-						"			username: '',\n" +
-						"     password: ''\n" +
-						"   },\n" +
-						"   methods: {\n" +
-						"     login: function(){\n" +
-						"       checklogin(this.username,this.password);" +
-						"     }\n" +
-						"   }\n" +
-						" })\n" +
-						" function checklogin(usrname,pwd) {\n" +
-						"   var xhr = new XMLHttpRequest();\n" +	
-						"// Register the embedded handler function\n" +	
-						"	  xhr.onreadystatechange = function () {\n" +	
-						"		if (xhr.readyState == 4 && xhr.status == 200) {\n" +	
-						"			var loginresult = xhr.responseText;\n" +	
-						"     alert(loginresult);" +	
-						"		}\n" +	
-						"	}\n" +	
-						"	xhr.open('GET', 'checklogin?username=' + usrname + '&password=' + pwd);\n" +	
-						"	xhr.send(null);\n" +	
-						" }\n" + 
-						"</script>" 
 						
 	  fmt.Fprintf(w,"%s",sHtml)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", web_response)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
